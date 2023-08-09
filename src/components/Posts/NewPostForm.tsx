7520a12c9193aa62927e32b5fd5af96dbcc02758
thakuralok99 <thakuralok99@gmail.com>
2023-08-09 20:51:54 +0530

    Post Form
    build post form

--- src/components/Posts/NewPostForm.tsx
+++ src/components/Posts/NewPostForm.tsx
@@ -4,10 +4,12 @@ import { BsLink45Deg, BsMic } from "react-icons/bs";
 import { IoDocumentText, IoImageOutline } from "react-icons/io5";
 import { BiPoll } from "react-icons/bi";
 import TabItem from "./TabItem";
+import TextInputs from "./PostForm/TextInputs";
+import ImageUpload from "./PostForm/ImageUpload";
 
 type NewPostFormProps = {};
 
-const formTabs = [
+const formTabs: TabItemType[] = [
   {
     title: "Post",
     icon: IoDocumentText,
@@ -37,16 +39,52 @@ export type TabItemType = {
 
 const NewPostForm: React.FC<NewPostFormProps> = () => {
   const [selectTab, setSelectedTab] = useState(formTabs[0].title);
+
+  const [textInputs, setTextInputs] = useState({
+    title: "",
+    body: "",
+  });
+  const [selectedFile, setSelectedFile] = useState<string>();
+  const [loading, setLoading] = useState(false);
+
+  const handleCreatePost = async () => {};
+
+  const onSelectImage = () => {};
+
+  const onTextChange = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const {
+      target: { name, value },
+    } = event;
+    setTextInputs((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
   return (
     <Flex direction="column" bg="white" borderRadius={4} mt={2}>
       <Flex width="100%">
         {formTabs.map((item) => (
           <TabItem
+            key={item.title}
             item={item}
             selected={item.title === selectTab}
             setSelectedTab={setSelectedTab}
           />
         ))}
+      </Flex>
+      <Flex p={4}>
+        {selectTab === "Post" && (
+          <TextInputs
+            textInput={textInputs}
+            handleCreatePost={handleCreatePost}
+            onChange={onTextChange}
+            loading={loading}
+          />
+        )}
+        {selectTab === "Images & Video" && <ImageUpload />}
       </Flex>
     </Flex>
   );
---
+++ src/components/Posts/PostForm/ImageUpload.tsx
@@ -0,0 +1,13 @@
+import { Flex } from "@chakra-ui/react";
+import React from "react";
+
+type ImageUploadProps = {};
+
+const ImageUpload: React.FC<ImageUploadProps> = () => {
+  return (
+    <Flex justify="center" align="center" width="100%">
+      Image Upload
+    </Flex>
+  );
+};
+export default ImageUpload;
